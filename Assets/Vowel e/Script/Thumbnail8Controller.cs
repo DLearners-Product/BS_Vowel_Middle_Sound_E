a2d5eb9cbb46425cd815235031af283da5628d26
Jaikishore <jaikishore@dlearners.in>
2024-11-11 18:29:02 +0530

    unwanted slides removed and 7th slide inprogress

---
+++ Assets/Vowel e/Script/Thumbnail8Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Thumbnail8Controller : MonoBehaviour
+{
+    public string[] questions;
+    public TextMeshProUGUI questionText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
